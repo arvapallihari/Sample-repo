a3bac3def5241780ffa5324c4d7c324fddea0104
arvapallihari <harvapalli9@gmail.com>
2020-06-18 16:34:33 +0530

    homeandlist

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  clickCounter: number = 100;
+  clickCounter: number = 0;
   name: string = '';
   constructor() { }
 
@@ -16,4 +16,12 @@ export class HomeComponent implements OnInit {
   countClick(){
   this.clickCounter  += 1;
 }
+setclasses() {
+  let myClasses = {
+    active: this.clickCounter > 4,
+    notactive: this.clickCounter <= 4,
+  }
+  return myClasses;
+}
+
 }
